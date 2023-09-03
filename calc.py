37a53ee7114425ccbdf76e4efc3328dcd8760d93
Rishi Kumar <130843510+Nxghtmar3@users.noreply.github.com>
2023-09-03 22:43:14 +0530

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Division by zero is not allowed."
+    else:
+      return a / b
+#you can call any of the functions above any number of times
